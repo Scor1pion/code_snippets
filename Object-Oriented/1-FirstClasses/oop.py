f46cfd820baf934c8b79482cae9637de36ef2fa7
Scor1pion <46466107+Scor1pion@users.noreply.github.com>
2019-02-02 22:03:48 -0500

    Update oop.py

--- Object-Oriented/1-FirstClasses/oop.py
+++ Object-Oriented/1-FirstClasses/oop.py
@@ -12,3 +12,13 @@ class Employee:
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
+
+#note the follow 2 statements are the same, emp_1 is the instant variable that is passed to self
+emp_1.fullname()
+Employee.fullname(emp_1)
+
+#the self is always the instance variable
+    def fullname():
+        return '{} {}'.format(self.first, self.last)
+ print(emp_fullname())
+#warning
